d44f47ffc6775e04cc34b3451d25e563a299a98b
Matthew Periut <18296791+Slainlight@users.noreply.github.com>
2020-03-14 21:45:31 -0400

    Mac OS compatibility
    Changed directories to work on both mac and windows. Linux probably works too ?

--- directories.py
+++ directories.py
@@ -2,8 +2,8 @@ import os
 import sys
 base = os.getcwd()
 
-statesFolder = base + "\\States"
-assetsFolder = base + "\\Assets"
+statesFolder = base + "/States"
+assetsFolder = base + "/Assets"
 
 sys.path.insert(1, statesFolder)
 sys.path.insert(1, assetsFolder)
